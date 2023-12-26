965f554108b2bd40acfd09bac6dc8cd22e7997be
Burcu Durmuş <burcudurmus@atlassian.com>
2023-12-26 10:56:49 +0300

    02.01 before prime react

--- frontend/src/YearContainer.js
+++ frontend/src/YearContainer.js
@@ -3,7 +3,7 @@
 // sonra ders seçtikten sonra printout kısmında o dersin prerequisitelerinin o course listesinde olup olmadığını kontrol etsin
 // eğer yoksa kırmızı renk varsa düz
 import React, {useEffect, useState} from 'react';
-
+import { Link } from 'react-router-dom';
 import { Galleria } from 'primereact/galleria';
         
 import styled from 'styled-components';
@@ -190,6 +190,33 @@ const YearContainer = () => {
       return unvalidCourses;
     };
 
+    const postData = async () => {
+      const url = 'http://localhost:5001/Control';
+    
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'text/plain', // Set the content type to JSON
+            // Add any additional headers if required
+          },
+          body: CreateRequestStructure(), // Replace with your actual data
+        });
+    
+        if (response.ok) {
+          console.log('Post request successful');
+          // Handle successful response here
+        } else {
+          console.error('Post request failed');
+          // Handle error response here
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+        // Handle network or other errors here
+      }
+    };
+    
+
     
   
     useEffect(() => {
@@ -223,7 +250,153 @@ const YearContainer = () => {
     
   }, []);
 
+  const [showDoubleMajorDropdown, setShowDoubleMajorDropdown] = useState(false);
+  const [showMinorDropdown, setShowMinorDropdown] = useState(false);
+
+  const Major = {"Computer Science": "BSCS","Economy": "BAECON", "Electrical Engineering": "BSEE", "Industrial Engineering": "BSMS", "Management": "BAMAN", "Materials Science and Nano Engineering":"BSMAT" ,"Mechatronics Engineering": "BSME","Molecular Biology, Genetics and Bioengineering": "BSBIO", "Political Science and International Relations": "BAPSIR", "Psychology": "BAPSY","Visual Arts and Visual Communications Desing": "BAVACD"};
+  const [selectedMajor, setSelectedMajor] = useState([]);
+  const EntryYear = {"2017 Fall": "201701", "2017 Spring": "201702", "2018 Fall": "201801", "2018 Spring": "201802", "2019 Fall": "201901", "2019 Spring": "201902", "2020 Fall": "202001", "2020 Spring": "202002", "2021 Fall": "202101", "2021 Spring": "202102","2022 Fall": "202201","2022 Spring": "202202", "2023 Fall": "2012301","2023 Spring": "202302"};
+  const [selectedYear, setSelectedYear] = useState([])
+  const [submitButton, setSubmitButton] = useState(false);
+
+  const submitButtonCheck = () => {
+    let finalRequestStructure  = [];
+    createSemesterStructure();
+    finalRequestStructure["EntryYear"] = selectedYear;
+    finalRequestStructure["Major"] = selectedMajor;
+    finalRequestStructure["Courses"] = reqStructure;
+    //console.log("reqStructure: ", semesterStructure)
+    
+    console.log("final: ",finalRequestStructure);
+    
+    if (selectedMajor.length !== 0 && selectedYear.length !== 0) {
+      setSubmitButton(true);
+      return true;
+      //postData();
+    } else {
+      console.log("Please select a major and entry year");
+
+      return false;
+    }
+
+  }
+
+
+  const toggleDoubleMajorDropdown = () => {
+    setShowDoubleMajorDropdown(!showDoubleMajorDropdown);
+  };
+
+  const toggleMinorDropdown = () => {
+    setShowMinorDropdown(!showMinorDropdown);
+  };
+
+  const MajorSelection = (event) => {
+    const selectMajor = event.target.value;
+    setSelectedMajor(selectMajor);
+    console.log("Major:", selectedMajor );  
+  };
+
+  const EntryYearSelection = (event) => {
+  const entry = event.target.value;
+    setSelectedYear(entry);
+    console.log("Entry Year:", selectedYear); 
+  };
+
+  const CreateRequestStructure = () => {
+    let finalRequestStructure  = [];
+    createSemesterStructure();
+    finalRequestStructure["EntryYear"] = selectedYear;
+    finalRequestStructure["Major"] = selectedMajor;
+    finalRequestStructure["Courses"] = reqStructure;
+    console.log("reqStructure: ", finalRequestStructure)
+    return finalRequestStructure;
+
+  };
+
+
     return (
+      <div>
+    <div className="info-bar">
+      <div className="dropdown">
+        <label htmlFor="entry-year" style={{ color: 'white' , fontWeight: 'bold'}}>EntryYear:</label>
+
+        <select onChange={(event) => EntryYearSelection(event)} className="custom-dropdown">
+              {Object.keys(EntryYear).map((course, index) => (
+                <option key={index} value={course}>
+                  {course}
+                </option>
+              ))}
+              
+        </select>
+
+      </div>
+      <div className="dropdown">
+        <label htmlFor="major" style={{ color: 'white' , fontWeight: 'bold'}} >Major:</label>
+        <select onChange={(event) => MajorSelection(event)} className="custom-dropdown">
+              {Object.keys(Major).map((course, index) => (
+                <option key={index} value={course}>
+                  {course}
+                </option>
+              ))}
+        </select>
+      </div>
+      <div className="dropdown" style={{ color: 'white' , fontWeight: 'bold'}}> Double Major:   
+      {showDoubleMajorDropdown ? (
+        // Render only when the dropdown button is shown
+        <select onChange={(event) => MajorSelection(event)} className="custom-dropdown">
+          {Object.keys(Major).map((course, index) => (
+            <option key={index} value={course}>
+              {course}
+            </option>
+          ))}
+        </select>
+      ) : (
+        // Render only when the dropdown button is NOT shown
+        <>
+          
+          <button className={`add-button ${showDoubleMajorDropdown ? 'active' : ''}`} onClick={toggleDoubleMajorDropdown}>
+            +
+          </button>
+        </>
+      )}
+      </div> 
+      <div className="dropdown" style={{ color: 'white' , fontWeight: 'bold'}}> Minor
+      {showMinorDropdown ? (
+        // Render only when the dropdown button is shown
+        
+        <select onChange={(event) => MajorSelection(event)} className="custom-dropdown">
+          {Object.keys(Major).map((course, index) => (
+            <option key={index} value={course}>
+              {course}
+            </option>
+          ))}
+        </select>
+      ) : (
+        // Render only when the dropdown button is NOT shown
+        <>
+          
+          <button className={`add-button ${showMinorDropdown ? 'active' : ''}`} onClick={toggleMinorDropdown}>
+            +
+          </button>
+        </>
+      )}
+       
+        </div>
+
+        
+        
+          
+        
+          <Link to="/tabmain" style={linkStyle}>
+          <button onClick={postData} style={buttonStyle}>Submit</button>
+        </Link>
+        
+
+        
+        
+      
+    </div>
+      
       <div className="year-container">
       <Container>
         <TableContainer>
@@ -380,7 +553,7 @@ const YearContainer = () => {
         </TableContainer>
       </Container>
       </div>
-      
+      </div>
     );
 
   
@@ -391,7 +564,18 @@ const YearContainer = () => {
 
 export default YearContainer;
 
+const linkStyle = {
+  textDecoration: 'none', // Remove default underline
+};
 
+const buttonStyle = {
+  backgroundColor: '#4CAF50', // Green background color
+  color: 'white', // White text color
+  padding: '10px 20px', // Padding for better appearance
+  border: 'none', // Remove border
+  borderRadius: '5px', // Add rounded corners
+  cursor: 'pointer', // Add pointer cursor on hover
+};
 
 const Container = styled.div`
   background-color:  #cf7fdf;
