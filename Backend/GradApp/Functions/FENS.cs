dea5eb7d5438575e82b5cd2054449a31a70a6428
umut ulas <umutulas@gilbarco.com.tr>
2024-01-14 19:15:29 +0300

    result page

--- Backend/GradApp/Functions/FENS.cs
+++ Backend/GradApp/Functions/FENS.cs
@@ -11,23 +11,42 @@ namespace GradApp.Functions
         public List<CreditResult> CalculateFENS(ProgramRequirement requirements, List<string> courses)
         {
             List<CreditResult> result = new List<CreditResult>();
+            Credit totalCredits = requirements.Credits.Where(x => x.Area == "Total").First();
+            CreditResult totalCreditResult = new CreditResult(totalCredits.Area, totalCredits.SU_Credit, totalCredits.Min_Course, totalCredits.AKTS_Credit);
 
             for (int i = 0; i < requirements.Requirements.Count(); i++)
             {
                 Credit credits = requirements.Credits.Where(x=>x.Area == requirements.Requirements[i].Area_Name).First();
-                CreditResult creditResult = new CreditResult(credits.Area, credits.SU_Credit, credits.AKTS_Credit, credits.Min_Course);
+                CreditResult creditResult = new CreditResult(credits.Area, credits.SU_Credit, credits.Min_Course, credits.AKTS_Credit);
                 List<CourseInfo> courseList = requirements.Requirements[i].Courses;
                 for (int k = 0; k < courseList.Count(); k++)
                 {
                     if (courses.Contains(courseList[k].Course_Name))
                     {
-                        if (creditResult.Success == true && (creditResult.Area == "Core Courses" || creditResult.Area == "Area Courses"))
+                        if (creditResult.Success == true && (creditResult.Area == "Core Electives" || creditResult.Area == "Area Electives"))
                         {
                             requirements.Requirements[i + 1].Courses.Insert(0, courseList[k]);
                         }
                         else
                         {
                             creditResult.increaseTaken(courseList[k]);
+                            if (creditResult.Area != "Basic Science" && creditResult.Area != "Engineering")
+                            {
+                                if (totalCreditResult.Min_Course != null)
+                                {
+                                    totalCreditResult.Taken_Course += 1;
+                                }
+                                if (totalCreditResult.SU_Credit != null)
+                                {
+                                    totalCreditResult.Taken_SU_Credit += courseList[k].SU_Credit;
+                                }
+                                if (totalCreditResult.AKTS_Credit != null)
+                                {
+                                    totalCreditResult.Taken_AKTS_Credit += courseList[k].AKTS_Credit;
+                                }
+
+                            }
+                            
                         }
                     }
                     else
@@ -38,30 +57,14 @@ namespace GradApp.Functions
                 result.Add(creditResult);
             }
 
-            Credit totalCredits = requirements.Credits.Where(x => x.Area == "Total").First();
-            CreditResult totalCreditResult = new CreditResult(totalCredits.Area, totalCredits.SU_Credit, totalCredits.AKTS_Credit, totalCredits.Min_Course);
-
-            for (int i = 0; i < result.Count(); i++)
-            {
-                if (totalCreditResult.Min_Course != null)
-                {
-                    totalCreditResult.Taken_Course += result[i].Taken_Course;
-                }
-                else if (totalCreditResult.SU_Credit != null)
-                {
-                    totalCreditResult.Taken_SU_Credit += result[i].Taken_SU_Credit;
-                }
-                else if (totalCreditResult.AKTS_Credit != null)
-                {
-                    totalCreditResult.Taken_AKTS_Credit += result[i].Taken_AKTS_Credit;
-                }
+            
                 
-            }
+            
             if (totalCreditResult.Min_Course >= totalCreditResult.Taken_Course && totalCreditResult.SU_Credit >= totalCreditResult.Taken_SU_Credit && totalCreditResult.AKTS_Credit >= totalCreditResult.Taken_AKTS_Credit)
             {
                 totalCreditResult.Success = true;
             }
-
+            result.Add(totalCreditResult);
             return result;
         }
     }
