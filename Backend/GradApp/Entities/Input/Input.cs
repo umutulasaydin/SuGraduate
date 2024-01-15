7f3cc1a1e1dedb939eb5a3d25963fcb0c649183f
umut ulas <umutulas@gilbarco.com.tr>
2024-01-15 07:11:19 +0300

    seperate backend

--- Backend/GradApp/Entities/Input/Input.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GradApp.Entities.Input
-{
-    public class Input
-    {
-        public List<string> Courses { get; set; }
-        public List<string> Programs { get; set; }
-        public string EntryYear { get; set; }
-    }
-}
--- Backend/GradApp/Entities/Requirements/Requirement.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GradApp.Entities.Requirements
-{
-    public class Requirement
-    {
-        public string Area_Name { get; set; }
-        public List<CourseInfo> Courses { get; set; }
-    }
-}
